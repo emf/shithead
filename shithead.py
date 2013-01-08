f2a2411d6d21ad117c495e4efd9f2e9d5186b4d8
Erik Fichtner <emf@obfuscation.org>
2013-01-08 07:33:13 -0600

    shithead; a tool to do bulk whois lookups over tor.
    you shouldn't do this. it violates terms-of-service for WHOIS providers. It's also probably not terribly friendly to the Tor
    network (although, it does inject a bunch of extra junk traffic, so who knows, maybe it frustrates people monitoring tor just a little?)
    But really. you shouldn't do this.
    
    socks.py is by Dan Haim. it's BSD licensed, and you can find it at socksipy.sourceforge.net if you want.

---
+++ shithead.py
@@ -0,0 +1,99 @@
+#
+# This is shithead. It does whois lookups over Tor. (or any socks5 proxy, i guess, but.. why?)
+# Why?  because I need to do a lot of lookups, and the registrars don't want me to.
+# I figured i'd try shitting this across Tor before I rented time on a botnet for this...
+#
+
+def whoRU():
+	while True:
+		name = q.get()
+		try:
+			w = pywhois.whois(name)
+			#epoch = int(time.mktime(pywhois.parser.cast_date(w.creation_date[0])))
+		except:
+			faults.put(name)
+			continue
+		finally:
+			q.task_done()
+
+		r.put( (name,w) )
+
+
+import socks
+import socket
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
+socket.socket = socks.socksocket
+import pywhois
+
+import time
+import sys
+from threading import Thread
+from Queue import Queue
+
+import sqlite3
+import jsonlib
+
+q = Queue()
+r = Queue()
+faults = Queue()
+
+print "starting threads..."
+for i in range(150):
+	t = Thread(target=whoRU)
+	t.daemon = True
+	t.start()
+
+print "fetching names..."
+names = []
+f = open("domains.txt","r")
+for line in f:
+	names.append( line.rstrip() )
+
+print "enqueueing whois queries..."
+for n in names:
+	q.put(n)
+
+print "waiting on queue..."
+while True:
+	sys.stdout.write("queue: " + str(q.unfinished_tasks) + "/" + str(q.qsize()) + " results: " + str(r.qsize()) + "\x1b[K\r")
+	if q.qsize() == 0 and q.unfinished_tasks == 0:
+		break
+	time.sleep(5)
+
+print "coalescing results..."
+results = []
+while r.empty() == False:
+	results.append(r.get_nowait())
+	r.task_done()
+
+print "dumping results to shit.db"
+
+conn = sqlite3.connect("shit.db")
+c = conn.cursor()
+try:
+	c.execute("create table whois(name varchar(255), response blob);")
+	c.execute("create index whois_dns on whois(name);")
+except:
+	print "Oh good, it appears to already be created..."
+	pass
+for o in results:
+	(name, whois) = o
+	w = jsonlib.write(dict([ (x, getattr(whois,x)) for x in whois.attrs() ]))
+	c.execute("insert into whois values(?,?)", (name, w))
+conn.commit()
+conn.close()
+
+print "coalescing faults..."
+results = []
+while faults.empty() == False:
+	results.append(faults.get_nowait())
+	faults.task_done()
+	
+output = open("faultlist.txt","w+")
+print "emitting faultlist.txt"
+for o in results:
+	output.write( o + "\n" )
+output.close()
+
+
+
